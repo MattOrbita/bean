d6d486ee30eaa07b9e7fcc866e7a74da29c74a31
MattOrbita <62462181+MattOrbita@users.noreply.github.com>
2024-10-08 10:42:39 -0700

    Add WASD movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,9 @@
-extends Node2D
+extends CharacterBody2D
+
+var move_speed = 500
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var direction = Input.get_vector('left', 'right', 'up', 'down')
+	velocity = direction * move_speed
+	move_and_slide()
