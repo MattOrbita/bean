90d01ed916195e11498470c15f08266c9c799c59
MattOrbita <62462181+MattOrbita@users.noreply.github.com>
2024-11-04 10:53:23 -0800

    Can feed and hurt minion, player has interact raycast
    Feeding a minion increases its health and size, hurting it does the opposite, feeding minion requires clicking on it and player ray cast hitting it

--- scripts/minion_tower.gd
+++ scripts/minion_tower.gd
@@ -4,16 +4,26 @@ extends CharacterBody2D
 var target : Node2D
 
 var health = 10
-var size
+var size = 1
+var health_per_size_unit = 50
 
 var move_speed = 200
 var attack_proximity = 150
 var can_attack : bool = true
 
+@onready var minion_sprite = $Sprite
+@onready var minion_collider = $CollisionShape2D
+
 @onready var attack_timer : Timer = $AttackTimer
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 
+# TODO figure out how to properly initialize vars below
 @onready var enemy_parent = $"../Enemies"
+@onready var player = $"../Player"
+
+
+func _ready() -> void:
+	update_size()
 
 
 func _process(delta: float) -> void:
@@ -42,7 +52,7 @@ func pursue_target():
 	
 	var direction = target.position - position
 	
-	if direction.length() > attack_proximity:
+	if direction.length() > attack_proximity: # TODO this'll be a problem the larger the minion is
 		velocity = move_speed * direction.normalized()
 		move_and_slide()
 	elif can_attack:
@@ -55,3 +65,28 @@ func pursue_target():
 
 func _on_timer_timeout() -> void:
 	can_attack = true
+
+
+func feed_minion(amt):
+	health += amt
+	update_size()
+
+
+func take_damage(amt):
+	health -= amt
+	if health <= 0:
+		queue_free()
+	
+	update_size()
+
+
+func update_size():
+	var new_size = 1 + float(health) / health_per_size_unit
+	
+	minion_sprite.set_scale(Vector2(new_size, new_size))
+	minion_collider.set_scale(Vector2(new_size, new_size))
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed('action') and player.get_pointing_to() == self:
+		feed_minion(10)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,29 @@
 extends CharacterBody2D
 
+
 var move_speed = 500
+var action_distance = 200
+
+@onready var raycast = $RayCast2D
 
 
 func _process(delta: float) -> void:
+	move()
+	point_raycast_to_cursor()
+
+
+func move():
 	var direction = Input.get_vector('left', 'right', 'up', 'down')
 	velocity = direction * move_speed
 	move_and_slide()
+
+
+func point_raycast_to_cursor():
+	var direction = get_global_mouse_position() - position
+	direction = direction.normalized()
+	
+	raycast.target_position = direction * action_distance
+
+
+func get_pointing_to():
+	return raycast.get_collider()
